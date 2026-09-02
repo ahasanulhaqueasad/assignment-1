67dd44cc5b3c60f23705fd7fb4335adc6657adff
ahasanulhaqueasad <ahasanulhaque204@gmail.com>
2026-09-02 14:01:06 +0600

    Second queston solved

--- answer.js
+++ answer.js
@@ -4,3 +4,26 @@ function describeValue(value) {
     const result = value ? "truthy" : "falsy"
     return `"${valueType} | ${result}"`;
 }
+// Question No. 2
+function getDayType(day) {
+    const newDay = day.toLowerCase().trim();
+
+    if (
+        newDay === "friday" ||
+        newDay === "saturday"
+    ) {
+        return "Weekend";
+    }
+    else if (
+        newDay === "sunday" ||
+        newDay === "monday" ||
+        newDay === "tuesday" ||
+        newDay === "wednesday" ||
+        newDay === "thursday"
+    ) {
+        return "Working Day"
+    }
+    else {
+        return "Invalid Day";
+    }
+}
