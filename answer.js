1c5b27eac5da84875e1f174c348d1b48108d3270
ahasanulhaqueasad <ahasanulhaque204@gmail.com>
2026-09-02 14:03:29 +0600

    forth question solved

--- answer.js
+++ answer.js
@@ -46,3 +46,20 @@ function validateUsername(username) {
         return "Available"
     }
 }
+
+
+// Question No. 4
+function validateUsername(username) {
+    if (username.length < 4) {
+        return "Too Short";
+    }
+    else if (username.includes(" ")) {
+        return "No Space Allowed";
+    }
+    else if (username.toLowerCase().includes("admin")) {
+        return "Reserved Word"
+    }
+    else {
+        return "Available"
+    }
+}
